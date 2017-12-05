0a3c9cf432505ca876ddc05516a4f318e2634d91
Keissy Leonardo <keissyleonardo@gmail.com>
2017-12-04 20:46:02 -0500

    commit

---
+++ assets/javascript/script.js
@@ -0,0 +1,65 @@
+// This is where the buttons will be added when doing the search 
+var puppiesVar = ["Yorkies", "Poodles", "Golden Retrievers", "Golden Doodles", "Mini Golden Doodles", "Pitbulls", "Pugs"]; 
+
+
+
+function renderButtons(){ 
+
+	$("#puppy-view").empty();
+
+	for ( var i = 0; i < puppiesVar.length; i++){
+		var a = $("<button>");
+		a.addClass('puppies');
+		a.attr("data-name", puppiesVar[i]); 
+		a.text(puppiesVar[i]); 
+		$("#puppy-view").append(a);
+	}
+
+	$("button").on("click", function(){
+		var dog = $(this).attr("data-name");
+		var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=eKcu9um6axWjo4h6ep6kNMAFBY7Z7bOG&q=" + dog + 
+		"&limit=25&offset=0&rating=G&lang=en"; 
+
+		$.ajax({
+			url: queryURL, 
+			method: "GET"
+		})
+
+		.done(function(response){
+
+			var results = response.data; 
+			for (var j = 0; j < results.length; j++){
+				var gifDiv = $("<div class = 'item'>"); 
+
+				var tittle = results[j].tittle;
+				var p = $("<p>").text("display_name" + tittle);
+
+				var dogImage = $("<img>"); 
+
+				dogImage.attr("src", results[j].images.fixed_height.url); 
+				gifDiv.prepend(dogImage);
+				gifDiv.prepend(p);
+				$("#gifs-appear-here").append(gifDiv); 
+
+			}
+
+
+		})
+
+
+	})
+
+}
+// On Click Function for the buttons to be added to the screen / form 
+
+$("#add-animal").on("click", function(event){
+
+	event.preventDefault();
+	var puppies = $("#puppy-input").val().trim();
+	puppiesVar.push(puppies);
+	renderButtons();
+
+}); 
+renderButtons();
+
+
